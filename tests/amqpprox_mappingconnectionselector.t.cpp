505f2237ee567bac0a8bffd37cce28b88177d594
Alaric <anightingale@bloomberg.net>
2021-06-22 18:25:52 +0100

    Add basic tests to mapping connection selector

--- tests/amqpprox_mappingconnectionselector.t.cpp
+++ tests/amqpprox_mappingconnectionselector.t.cpp
@@ -14,16 +14,98 @@
 ** limitations under the License.
 */
 
+#include <amqpprox_robinbackendselector.h>
+#include <amqpprox_connectionmanager.h>
+#include <amqpprox_sessionstate.h>
 #include <amqpprox_mappingconnectionselector.h>
+#include <amqpprox_farmstore.h>
+#include <amqpprox_backendstore.h>
+#include <amqpprox_resourcemapper.h>
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-
 
 using namespace Bloomberg;
 using namespace amqpprox;
 using namespace testing;
 
 TEST(MappingConnectionSelector, Breathing) {
-    // TODO
+    FarmStore farmStore;
+    BackendStore backendStore;
+    ResourceMapper resourceMapper;
+    MappingConnectionSelector connectionSelector(&farmStore, &backendStore, &resourceMapper);
+}
+
+TEST(MappingConnectionSelector, Find_Nothing) {
+    FarmStore farmStore;
+    BackendStore backendStore;
+    ResourceMapper resourceMapper;
+    MappingConnectionSelector connectionSelector(&farmStore, &backendStore, &resourceMapper);
+    SessionState state;
+    std::shared_ptr<ConnectionManager> out;
+    EXPECT_EQ(connectionSelector.acquireConnection(&out, state), 1);
+}
+
+TEST(MappingConnectionSelector, Find_Nothing_Defaulted) {
+    FarmStore farmStore;
+    BackendStore backendStore;
+    ResourceMapper resourceMapper;
+    MappingConnectionSelector connectionSelector(&farmStore, &backendStore, &resourceMapper);
+
+    connectionSelector.setDefaultFarm("DEFAULT");
+    SessionState state;
+    std::shared_ptr<ConnectionManager> out;
+    EXPECT_EQ(connectionSelector.acquireConnection(&out, state), 4);
+
+    // Check after unsetting the default we go back to the previous rc
+    connectionSelector.unsetDefaultFarm();
+    EXPECT_EQ(connectionSelector.acquireConnection(&out, state), 1);
+}
+
+TEST(MappingConnectionSelector, Successful_Farm_Find) {
+    FarmStore farmStore;
+    BackendStore backendStore;
+    ResourceMapper resourceMapper;
+    RobinBackendSelector backendSelector;
+    std::vector<std::string> members;
+    farmStore.addFarm(std::make_unique<Farm>("DEFAULT", members, &backendStore, &backendSelector));
+    MappingConnectionSelector connectionSelector(&farmStore, &backendStore, &resourceMapper);
+
+    connectionSelector.setDefaultFarm("DEFAULT");
+    SessionState state;
+    std::shared_ptr<ConnectionManager> out;
+    EXPECT_EQ(connectionSelector.acquireConnection(&out, state), 0);
+}
+
+TEST(MappingConnectionSelector, Find_Nothing_Backend) {
+    FarmStore farmStore;
+    BackendStore backendStore;
+    ResourceMapper resourceMapper;
+
+    MappingConnectionSelector connectionSelector(&farmStore, &backendStore, &resourceMapper);
+    SessionState state;
+
+    state.setVirtualHost("/");
+    resourceMapper.mapVhostToBackend("/", "non-existing");
+    std::shared_ptr<ConnectionManager> out;
+    EXPECT_EQ(connectionSelector.acquireConnection(&out, state), 3);
+}
+
+TEST(MappingConnectionSelector, Successful_Backend_Find) {
+    FarmStore farmStore;
+    BackendStore backendStore;
+    ResourceMapper resourceMapper;
+    RobinBackendSelector backendSelector;
+
+    Backend backend1(
+        "backend1", "dc1", "backend1.bloomberg.com", "127.0.0.1", 5672, true);
+    backendStore.insert(backend1);
+
+    MappingConnectionSelector connectionSelector(&farmStore, &backendStore, &resourceMapper);
+    SessionState state;
+
+    state.setVirtualHost("/");
+    resourceMapper.mapVhostToBackend("/", "backend1");
+    std::shared_ptr<ConnectionManager> out;
+    EXPECT_EQ(connectionSelector.acquireConnection(&out, state), 0);
 }
